8cd6d4c09f0d4d2542ff73965504d7fbba9c06bf
Eric Chen <eric@ericchen.me>
2013-05-04 16:32:30 -0700

    gogo

--- public/app/controllers/controllers.js
+++ public/app/controllers/controllers.js
@@ -1,16 +1,4 @@
-﻿/*#######################################################################
-  
-  Dan Wahlin
-  http://twitter.com/DanWahlin
-  http://weblogs.asp.net/dwahlin
-  http://pluralsight.com/training/Authors/Details/dan-wahlin
-
-  Normally like the break AngularJS controllers into separate files.
-  Kept them together here since they're small and it's easier to look through them.
-  example. 
-
-  #######################################################################*/
-
+﻿
 
 //This controller retrieves data from the customersService and associates it with the $scope
 //The $scope is ultimately bound to the customers view
